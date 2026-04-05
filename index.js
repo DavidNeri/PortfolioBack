2aae09d583cc949607ec20add12d38ecdbfe26f5
David Christian Neri <50834481+DavidNeri@users.noreply.github.com>
2026-04-05 17:53:02 -0300

    puppeter config render

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ var corsOptions = {
 }
 
 app.use(cors(corsOptions))
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+
 app.use(express.json())
 
 
